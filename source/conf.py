0950b086f07d3adea4f87bb9b05e849f224643b9
Kārlis Seņko <karlis3p70l1ij@gmail.com>
2020-08-18 17:47:23 +0300

    Add icon.

--- source/conf.py
+++ source/conf.py
@@ -84,7 +84,7 @@ autosectionlabel_maxdepth = 2
 #
 html_theme = 'cutter_theme'
 html_theme_path = ['..']
-html_logo = '../../src/img/cutter.ico'
+html_logo = 'images/cutter.ico'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
